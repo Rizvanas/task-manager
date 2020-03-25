b9cd94025cf5611b541afb196d0c014f5ebdb092
Rizvan <rizvanchalilovas@gmail.com>
2020-03-25 16:04:53 +0200

    Scaffold shared services

---
+++ src/shared/constants.js
@@ -0,0 +1,3 @@
+export const inputDateFormat = 'YYY-MM-DD';
+export const displayDateFormat = 'MMM DD YYYY';
+export const displayTimeFormat = 'HH:mm';
---
+++ src/views/projects.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'Projects',
+  data() {
+    return {};
+  },
+  computed: {},
+};
+</script>
