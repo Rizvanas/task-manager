f0b4073dc6d372a1d6a5f63f05f12d1b05722692
Rizvan <rizvanchalilovas@gmail.com>
2020-04-02 00:17:01 +0300

    Refactor goal action addition: action form into separate component

--- src/components/add-goal-card.vue
+++ src/components/add-goal-card.vue
@@ -61,67 +61,7 @@
               <span>Add new action</span>
             </a>
           </div>
-          <div v-if="addNewAction" class="box has-border">
-            <div class="card-content">
-              <div class="field has-addons">
-                <div class="control">
-                  <b-dropdown
-                    position="is-left"
-                    append-to-body
-                    aria-role="menu"
-                    trap-focus
-                  >
-                    <a class="button" slot="trigger" role="button">
-                      <span>{{ currentActionEmoji }}</span>
-                    </a>
-                    <b-dropdown-item :focusable="false" custom>
-                      <div class="has-shadow" style="width:242px;">
-                        <b-field>
-                          <b-slider
-                            label="Time action will take"
-                            type="is-info"
-                            :min="0"
-                            :max="10"
-                            :step="0.1"
-                            :custom-formatter="emojiTooltip"
-                            tooltip-type="is-white"
-                            v-model="sliderValue"
-                            lazy
-                          >
-                            <template v-for="val in [0, 2, 4, 6, 8, 10]">
-                              <b-slider-tick :value="val" :key="val">
-                                {{ val }}
-                              </b-slider-tick>
-                            </template>
-                          </b-slider>
-                        </b-field>
-                      </div>
-                    </b-dropdown-item>
-                  </b-dropdown>
-                </div>
-                <p class="control">
-                  <input
-                    type="text"
-                    class="input"
-                    placeholder="Name your action"
-                  />
-                </p>
-              </div>
-            </div>
-            <div class="buttons is-centered">
-              <button class="button is-primary is-rounded">
-                <span class="icon is-small">
-                  <i class="fas fa-save"></i>
-                </span>
-                <span>Save</span>
-              </button>
-              <button class="button is-danger is-rounded is-outlined">
-                <span class="icon is-small">
-                  <i class="fas fa-times"></i>
-                </span>
-              </button>
-            </div>
-          </div>
+          <AddActionForm v-if="addNewAction" />
         </b-step-item>
 
         <template
@@ -154,17 +94,19 @@
 </template>
 
 <script>
-import { getWorkHoursInEmoji } from '@/shared/constants';
+import AddActionForm from '@/components/add-action-form';
 
 export default {
   name: 'AddGoalCard',
+
+  components: { AddActionForm },
+
   data() {
     return {
       deadline: new Date(),
       gotSelected: false,
       showDatepicker: false,
       addNewAction: false,
-      sliderValue: 1,
     };
   },
 
@@ -185,18 +127,11 @@ export default {
     toggleShowDatepicker() {
       this.$refs.datepicker.toggle();
     },
-    emojiTooltip(val) {
-      const emoji = getWorkHoursInEmoji(val);
-      return emoji;
-    },
   },
   computed: {
     minDate: function() {
       const today = new Date();
       return new Date(today.getFullYear(), today.getMonth(), today.getDate());
-    },
-    currentActionEmoji: function() {
-      return getWorkHoursInEmoji(this.sliderValue);
     },
   },
 };
