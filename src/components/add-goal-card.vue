48ad9252592c71ffcb40fb18b875f05360b23452
Rizvan <rizvanchalilovas@gmail.com>
2020-04-01 21:57:26 +0300

    Add emoji rating in new action addition form

--- src/components/add-goal-card.vue
+++ src/components/add-goal-card.vue
@@ -61,68 +61,67 @@
               <span>Add new action</span>
             </a>
           </div>
-          <transition appear>
-            <div v-if="addNewAction" class="box has-border">
-              <div class="card-content">
-                <div class="field has-addons">
-                  <div class="control">
-                    <b-dropdown
-                      position="is-left"
-                      append-to-body
-                      aria-role="menu"
-                      trap-focus
-                    >
-                      <a class="button" slot="trigger" role="button">
-                        <span>💩</span>
-                      </a>
-                      <b-dropdown-item :focusable="false" custom>
-                        <div class="has-shadow" style="width:242px;">
-                          <b-field>
-                            <b-slider
-                              label="Time action will take"
-                              type="is-info"
-                              :min="0"
-                              :max="10"
-                              :step="0.1"
-                              :custom-formatter="emojiTooltip"
-                              tooltip-type="is-white"
-                              v-model="sliderValue"
-                            >
-                              <template v-for="val in [0, 2, 4, 6, 8, 10]">
-                                <b-slider-tick :value="val" :key="val">{{
-                                  val
-                                }}</b-slider-tick>
-                              </template>
-                            </b-slider>
-                          </b-field>
-                        </div>
-                      </b-dropdown-item>
-                    </b-dropdown>
-                  </div>
-                  <p class="control">
-                    <input
-                      type="text"
-                      class="input"
-                      placeholder="Name your action"
-                    />
-                  </p>
+          <div v-if="addNewAction" class="box has-border">
+            <div class="card-content">
+              <div class="field has-addons">
+                <div class="control">
+                  <b-dropdown
+                    position="is-left"
+                    append-to-body
+                    aria-role="menu"
+                    trap-focus
+                  >
+                    <a class="button" slot="trigger" role="button">
+                      <span>{{ currentActionEmoji }}</span>
+                    </a>
+                    <b-dropdown-item :focusable="false" custom>
+                      <div class="has-shadow" style="width:242px;">
+                        <b-field>
+                          <b-slider
+                            label="Time action will take"
+                            type="is-info"
+                            :min="0"
+                            :max="10"
+                            :step="0.1"
+                            :custom-formatter="emojiTooltip"
+                            tooltip-type="is-white"
+                            v-model="sliderValue"
+                            lazy
+                          >
+                            <template v-for="val in [0, 2, 4, 6, 8, 10]">
+                              <b-slider-tick :value="val" :key="val">
+                                {{ val }}
+                              </b-slider-tick>
+                            </template>
+                          </b-slider>
+                        </b-field>
+                      </div>
+                    </b-dropdown-item>
+                  </b-dropdown>
                 </div>
-              </div>
-              <div class="buttons is-centered">
-                <button class="button is-primary is-rounded">
-                  <span class="icon is-small">
-                    <i class="fas fa-save"></i>
-                  </span>
-                  <span>Save</span>
-                </button>
-                <button class="button is-danger is-rounded is-outlined">
-                  <span class="icon is-small">
-                    <i class="fas fa-times"></i>
-                  </span>
-                </button>
+                <p class="control">
+                  <input
+                    type="text"
+                    class="input"
+                    placeholder="Name your action"
+                  />
+                </p>
               </div>
             </div>
-          </transition>
+            <div class="buttons is-centered">
+              <button class="button is-primary is-rounded">
+                <span class="icon is-small">
+                  <i class="fas fa-save"></i>
+                </span>
+                <span>Save</span>
+              </button>
+              <button class="button is-danger is-rounded is-outlined">
+                <span class="icon is-small">
+                  <i class="fas fa-times"></i>
+                </span>
+              </button>
+            </div>
+          </div>
         </b-step-item>
 
         <template
@@ -156,6 +155,7 @@
 
 <script>
 import { getWorkHoursInEmoji } from '@/shared/constants';
+
 export default {
   name: 'AddGoalCard',
   data() {
@@ -191,10 +191,12 @@ export default {
     },
   },
   computed: {
-    minDate() {
+    minDate: function() {
       const today = new Date();
-
       return new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    },
+    currentActionEmoji: function() {
+      return getWorkHoursInEmoji(this.sliderValue);
     },
   },
 };
