b4ee5bc2b44746541fa0da4b5d0d6066f8694768
Rizvan <rizvanchalilovas@gmail.com>
2020-04-01 18:41:08 +0300

    Add drag and drop functionality

--- src/views/goals.vue
+++ src/views/goals.vue
@@ -2,24 +2,46 @@
   <section class="section">
     <div class="hero">
       <div class="hero-body">
-        <h1 class="title is-size-1 has-text-centered has-text-weight-bold">
+        <h1 class="title is-size-1 has-text-weight-bold">
           <span class="underline-primary">Goals</span>
+          <span>
+            <button
+              @click="enterAddNewCardMode"
+              class="button button-special is-rounded"
+            >
+              <span class="icon is-small">
+                <i class="fas fa-plus"></i>
+              </span>
+              <span>Set new goal</span>
+            </button>
+          </span>
         </h1>
       </div>
     </div>
-    <div class="columns is-multiline">
+
+    <AddGoalCard class="column is-12-tablet is-6-desktop is-4-widescreen" />
+    <draggable
+      class="columns is-multiline"
+      v-model="goals"
+      v-bind="dragOptions"
+      @start="drag = true"
+      @end="drag = false"
+      tag="div"
+    >
       <div
         v-for="goal in goals"
-        :key="goal.id"
+        :key="goal.order"
         class="column is-12-tablet is-6-desktop is-4-widescreen"
       >
         <GoalCard :goal="goal" />
       </div>
-    </div>
+    </draggable>
   </section>
 </template>
 <script>
 import GoalCard from '@/components/goal-card.vue';
+import AddGoalCard from '@/components/add-goal-card.vue';
+import draggable from 'vuedraggable';
 
 const goals = [
   { id: 1, title: 'Mano tikslas' },
@@ -32,12 +54,58 @@ const goals = [
 
 export default {
   name: 'Goals',
+  display: 'Transitions',
+  components: { GoalCard, AddGoalCard, draggable },
+
   data() {
     return {
-      goals,
+      goals: goals.map((goal, index) => {
+        return { ...goal, order: index + 1 };
+      }),
+      drag: false,
+      addingNewCard: false,
     };
   },
-  computed: {},
-  components: { GoalCard },
+  methods: {
+    sort() {
+      this.goals = this.goals.sort((a, b) => a.order - b.order);
+    },
+    enterAddNewCardMode() {},
+  },
+  computed: {
+    dragOptions() {
+      return {
+        animation: 150,
+        swapTreshold: 1,
+        group: 'description',
+        disabed: false,
+        ghostClass: 'ghost',
+        sort: true,
+      };
+    },
+  },
 };
 </script>
+
+<style scoped>
+.flip-list-move {
+  transition: transform 0.5s;
+}
+
+.no-move {
+  opacity: 0.5s;
+}
+
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+
+.list-group-item {
+  cursor: move;
+}
+
+.list-group-item i {
+  cursor: pointer;
+}
+</style>
