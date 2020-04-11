473f8c53bb66aff1693f04f45ea6da44c1efe45d
Rizvan <rizvanchalilovas@gmail.com>
2020-04-11 22:02:49 +0300

    Create a separate finished action state component

---
+++ src/components/finished-action.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class="level">
+      <div class="level-left">
+        <div class="level-item has-text-centered">
+          <p class="title">{{ currentActionEmoji }}</p>
+        </div>
+      </div>
+      <transition name="fade">
+        <div class="level-item">
+          <p class="action-text">{{ clonedAction.title }}</p>
+        </div>
+      </transition>
+      <div class="level-right" tabindex="-1">
+        <div class="level-item">
+          <p>Uzbaigimo statistika</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ActionMixins from '@/mixins/action-mixins';
+
+export default {
+  name: 'FinishedAction',
+
+  props: {
+    action: {
+      type: Object,
+      default: () => {
+        return {
+          title: '',
+          workHours: 0,
+        };
+      },
+    },
+  },
+
+  mixins: [ActionMixins],
+
+  data() {
+    return {
+      clonedAction: { ...this.action },
+    };
+  },
+};
+</script>
