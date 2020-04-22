030f75ec72f51837b99048d0995f3333aa787a33
Rizvan <rizvanchalilovas@gmail.com>
2020-04-22 21:25:27 +0300

    Add friend list component

---
+++ src/components/FriendList.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <div class="level is-marginless">
+      <div class="level-item level-right">
+        <a
+          @mouseenter="hovered = true"
+          @mouseleave="hovered = false"
+          class="edit-button"
+        >
+          <i class="fas fa-user-plus"></i>
+        </a>
+      </div>
+    </div>
+    <p class="is-size-5 has-text-weight-bold tile-title">Friends</p>
+    <perfect-scrollbar>
+      <ul class="menu menu-list">
+        <FriendListItem />
+        <FriendListItem />
+        <FriendListItem />
+        <FriendListItem />
+        <FriendListItem />
+        <FriendListItem />
+        <FriendListItem />
+        <FriendListItem />
+        <FriendListItem />
+      </ul>
+    </perfect-scrollbar>
+  </div>
+</template>
+
+<script>
+import FriendListItem from '@/components/FriendListItem';
+export default {
+  name: 'FriendList',
+
+  components: {
+    FriendListItem,
+  },
+
+  data() {
+    return {
+      hovered: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.tile-title {
+  margin: 0 0 1em 1em;
+}
+</style>
---
+++ src/components/FriendListItem.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="level friend-item">
+    <div class="level-left">
+      <div class="level-item">
+        <span class="profile-pic image is-32x32">
+          <img class="is-rounded" :src="friend.avatar" alt="Friend avatar" />
+        </span>
+      </div>
+      <div class="level-item">
+        <p class="is-size-6">{{ friend.username }}</p>
+      </div>
+    </div>
+    <transition name="fade">
+      <div class="level-right is-size-7">
+        <p class="is-small is-pulled-right action-button--submit">
+          <span class="icon">
+            <i class="fas fa-project-diagram"></i>
+          </span>
+        </p>
+        <p class="is-small is-pulled-right action-button--cancel">
+          <span class="icon">
+            <i class="fas fa-times"></i>
+          </span>
+        </p>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FieldListItem',
+  props: {
+    friend: {
+      type: Object,
+      default: () => {
+        return {
+          username: 'Rizvanas',
+          avatar: 'https://bulma.io/images/placeholders/96x96.png',
+        };
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.friend-item {
+  margin: 4px 20px;
+  padding: 0 0.5em 0 0.5em;
+  height: 42px;
+  border-radius: 6px;
+  transition: all 300ms cubic-bezier(0, 0.09, 0.33, 0.97);
+
+  &:hover {
+    box-shadow: 0px 4px 8px rgba(38, 38, 38, 0.2);
+    background-color: #f6f9fc;
+
+    .action-button {
+      background-color: #cde3f8;
+    }
+  }
+
+  .action-button {
+    width: 32px;
+    height: 32px;
+    padding: 0.5em 0.75em;
+    border-radius: 6px;
+    margin-left: 0.5em;
+    transition: all 300ms cubic-bezier(0, 0.09, 0.33, 0.97);
+    position: relative;
+
+    i {
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      display: block;
+    }
+    &:hover {
+      i {
+        color: #fff;
+      }
+    }
+
+    &--submit {
+      @extend .action-button;
+      &:hover {
+        background-color: #3273dc;
+      }
+    }
+
+    &--cancel {
+      @extend .action-button;
+      &:hover {
+        background-color: #d33e5d;
+      }
+    }
+  }
+}
+.profile-pic {
+  margin-right: 1em;
+}
+.ps {
+  height: 420px;
+}
+</style>
